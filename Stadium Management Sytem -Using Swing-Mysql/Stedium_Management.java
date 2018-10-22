b19f7459afa58d1c21e59960cda7e650e25c9aca
Soumyadip Chowdhury <37873518+soumyadip007@users.noreply.github.com>
2018-10-23 02:00:36 +0530

    Add files via upload

---
+++ Stadium Management Sytem -Using Swing-Mysql/Stedium_Management.java
@@ -0,0 +1,11 @@
+package Stedium_Management;
+
+public class Stedium_Management {
+
+	public static void main(String args[])
+	{
+		MainScreen ms=new MainScreen();
+		ms.setTitle("Stadium Management System");
+		ms.setVisible(true);
+	}
+}
